865deade12c0c8cb5ed8bc376f9763b777de301f
linxueyu <lin2006yuo@gmail.com>
2020-10-15 01:41:51 +0800

    demo

--- src/g6.js
+++ src/g6.js
@@ -1,16 +1,22 @@
 import G6 from "@antv/g6"
+import { mat3 } from "@antv/matrix-util"
 import { transferG6Path } from "./utils"
 
+const round_w = 80 // 物料节点宽
+const round_h = 50 // 物料节点高
+const node_radius = 25 // 物料节点圆角
+
+// 拓展一个圆角rect
 G6.registerNode(
   "round",
   {
     draw(cfg, group) {
       const color = cfg.color
-      const w = cfg.size[0] || 20
-      const h = cfg.size[1] || 20
+      const w = cfg.size[0] || round_w
+      const h = cfg.size[1] || round_h
       const x = cfg.x || 0
       const y = cfg.y || 0
-      const r = cfg.r
+      const r = cfg.r // 圆角半径
 
       const path = [
         ["M", x + r, y],
@@ -22,24 +28,22 @@ G6.registerNode(
         ["Q", x, y + h, x, y + h - r],
         ["L", x, y + r],
         ["Q", x, y, x + r, y],
-        ["Z"]
+        ["Z"],
       ]
       const style = G6.Util.mix(
         {},
         {
           path: path,
-          lineWidth: 2,
-          fill: "red",
-          stroke: color
+          fill: color,
         },
         cfg.style
       )
       const keyShape = group.addShape("path", {
         attrs: {
-          ...style
+          ...style,
         },
         draggable: true,
-        name: "round"
+        name: "round",
       })
 
       // 三角图标
@@ -50,17 +54,13 @@ G6.registerNode(
           points: [
             [tri_point[0], tri_point[1]],
             [tri_point[0] + tri_r, tri_point[1] + tri_r / 2],
-            [tri_point[0], tri_point[1] + tri_r]
+            [tri_point[0], tri_point[1] + tri_r],
           ],
-          fill: "blue"
-        }
+          fill: "blue",
+        },
       })
 
       if (cfg.label) {
-        // 如果有文本
-        // 如果需要复杂的文本配置项，可以通过 labeCfg 传入
-        // const style = (cfg.labelCfg && cfg.labelCfg.style) || {};
-        // style.text = cfg.label;
         const label = group.addShape("text", {
           attrs: {
             x: 0 + w + 12, // 居中
@@ -68,20 +68,18 @@ G6.registerNode(
             textAlign: "left",
             textBaseline: "middle",
             text: cfg.label,
-            fill: "#666"
+            fill: "#666",
           },
           // must be assigned in G6 3.3 and later versions. it can be any value you want
           name: "text-shape",
           // 设置 draggable 以允许响应鼠标的图拽事件
-          draggable: true
+          draggable: true,
         })
       }
 
-      // 返回 keyShape
       return keyShape
-    }
+    },
   },
-  // 注意这里继承了 'single-node'
   "single-node"
 )
 
@@ -91,8 +89,8 @@ G6.registerNode("work-node", {
       {},
       {
         r: cfg.r,
-        x: 10, // 10是round节点的w
-        y: 10 // 10是round节点的h
+        x: round_w / 2, // 10是round节点的w
+        y: round_h / 2, // 10是round节点的h
       },
       cfg.style
     )
@@ -100,38 +98,39 @@ G6.registerNode("work-node", {
       attrs: {
         ...style,
         stroke: "red",
-        fill: cfg.state === "UNDO" || cfg.state === "DOING" ? "" : "red",
-        lineWidth: 2
-      }
+        cursor: 'pointer',
+        fill: cfg.state === "UNDO" || cfg.state === "DOING" ? "white" : "red",
+        lineWidth: 2,
+      },
     })
 
     // 三角图标
     const w = cfg.r * 2
     const h = cfg.r * 2
     const tri_r = 5
-    const tri_point = [w / 2 + 10 + 4, h / 2 - tri_r / 2 - 10] // 10是round节点的w，h
+    const tri_point = [w / 2 + round_w / 2 + 4, round_h / 2 - tri_r / 2] // 10是round节点的w，h
     group.addShape("polygon", {
       attrs: {
         points: [
           [tri_point[0], tri_point[1]],
           [tri_point[0] + tri_r, tri_point[1] + tri_r / 2],
-          [tri_point[0], tri_point[1] + tri_r]
+          [tri_point[0], tri_point[1] + tri_r],
         ],
-        fill: "blue"
-      }
+        fill: "blue",
+      },
     })
 
     if (cfg.state === "UNDO") {
       group.addShape("path", {
         attrs: {
           path: [
-            ["M", 10 - cfg.r / 2, 10], // 10是round节点的w，h
-            ["L", 10 + cfg.r / 2, 10]
+            ["M", round_w / 2 - cfg.r / 2, round_h / 2], // 10是round节点的w，h
+            ["L", round_w / 2 + cfg.r / 2, round_h / 2],
           ],
           lineCap: "round",
           lineWidth: 4,
-          stroke: "red"
-        }
+          stroke: "red",
+        },
       })
     }
 
@@ -156,85 +155,102 @@ G6.registerNode("work-node", {
      * z
      */
     if (cfg.state === "DOING") {
-      const ratio = (cfg.r * 2) / 226.8 / 1.2
+      const ratio = (cfg.r * 2) / 226.8 / 1.2 // 226.8为svg viewBox值
+      // 转化为G6要求的格式
       const path1 = transferG6Path(
-        "M113,34.1V8c0-1.2-1.4-1.8-2.2-0.9L74.8,43c-0.5,0.5-0.5,1.4,0,1.9l36,36c0.9,0.9,2.2,0.2,2.2-0.9V55.1  c0-0.7,0.6-1.3,1.4-1.3c31.7,0.7,57.4,26.9,57.4,58.7c0,9.4-2.7,18.9-6.5,26.6c-0.2,0.5-0.2,1.1,0.2,1.5l12.8,12.8  c0.6,0.6,1.7,0.5,2.1-0.3c6.4-12.3,10.9-25.5,10.9-40.5C191.3,69.4,156.1,34.1,113,34.1L113,34.1z",
+        `M113,34.1V8c0-1.2-1.4-1.8-2.2-0.9L74.8,43c-0.5,0.5-0.5,1.4,0,1.9l36,36c0.9,0.9,2.2,0.2,2.2-0.9V55.1  c0-0.7,0.6-1.3,1.4-1.3c31.7,0.7,57.4,26.9,57.4,58.7c0,9.4-2.7,18.9-6.5,26.6c-0.2,0.5-0.2,1.1,0.2,1.5l12.8,12.8  c0.6,0.6,1.7,0.5,2.1-0.3c6.4-12.3,10.9-25.5,10.9-40.5C191.3,69.4,156.1,34.1,113,34.1L113,34.1z`,
         ratio
       )
+
       const path2 = transferG6Path(
         "M113,169.9c0,0.7-0.6,1.3-1.4,1.3c-31.7-0.7-57.4-26.9-57.4-58.7c0-9.5,2.7-18.9,6.5-26.6  c0.2-0.5,0.2-1.1-0.2-1.5L47.8,71.6c-0.6-0.6-1.7-0.5-2.1,0.3c-6.4,12.3-10.9,25.5-10.9,40.5c0,42.6,34.5,77.6,77,78.3  c0.7,0,1.3,0.6,1.3,1.3V217c0,1.2,1.4,1.8,2.2,0.9l36-36c0.5-0.5,0.5-1.4,0-1.9l-36-36c-0.9-0.9-2.2-0.2-2.2,0.9L113,169.9  L113,169.9z",
         ratio
       )
-      group.addShape("path", {
+
+      const p1 = group.addShape("path", {
         attrs: {
           path: path1,
           fill: "red",
-          lineWidth: 2,
-          stroke: "red"
-        }
+          lineWidth: 1,
+        },
       })
-      group.addShape("path", {
+      let matrix1 = p1.getMatrix()
+      if (!matrix1) matrix1 = mat3.create()
+      const newMatrix1 = G6.Util.transform(matrix1, [
+        [
+          "t",
+          (-226.8 * ratio) / 2 + round_w / 2,
+          (-226.8 * ratio) / 2 + round_h / 2,
+        ], // translate
+      ])
+      p1.setMatrix(newMatrix1)
+
+      const p2 = group.addShape("path", {
         attrs: {
           path: path2,
           fill: "red",
-          lineWidth: 2,
-          stroke: "red"
-        }
+          lineWidth: 1,
+        },
       })
+      let matrix2 = p2.getMatrix()
+      if (!matrix2) matrix2 = mat3.create()
+      const newMatrix2 = G6.Util.transform(matrix2, [
+        [
+          "t",
+          (-226.8 * ratio) / 2 + round_w / 2,
+          (-226.8 * ratio) / 2 + round_h / 2,
+        ], // translate
+      ])
+      p2.setMatrix(newMatrix2)
     }
 
     if (cfg.state === "DONE") {
-      const point = 8 // 这里是round节点的w
+      const point_x = round_w / 2 - 2 // 这里是round节点的w
+      const point_y = round_h / 2 - 2 // 这里是round节点的w
       group.addShape("path", {
         attrs: {
           path: [
-            ["M", point - cfg.r / 2 + 2, point + 2],
-            ["L", point, point + cfg.r / 3 + 2],
-            ["L", point + cfg.r / 2 + 2, point - cfg.r / 2 + 2 + 2]
+            ["M", point_x - cfg.r / 2 + 2, point_y + 2],
+            ["L", point_x, point_y + cfg.r / 3 + 2],
+            ["L", point_x + cfg.r / 2 + 2, point_y - cfg.r / 2 + 2 + 2],
           ],
           lineCap: "round",
           lineJoin: "round",
           lineWidth: 4,
-          stroke: "white"
-        }
+          stroke: "white",
+        },
       })
     }
 
     if (cfg.label) {
-      // 如果有文本
-      // 如果需要复杂的文本配置项，可以通过 labeCfg 传入
-      // const style = (cfg.labelCfg && cfg.labelCfg.style) || {};
-      // style.text = cfg.label;
       const label = group.addShape("text", {
-        // attrs: style
         attrs: {
-          x: cfg.r + 10 + 12, // 居中 10 是round节点的w，h
-          y: cfg.r / 2,
+          x: cfg.r + round_w / 2 + 12,
+          y: cfg.r / 2 + round_h / 2 - 10,
           textAlign: "left",
           textBaseline: "middle",
           text: cfg.label,
-          fill: "#666"
+          fill: "#666",
         },
         // must be assigned in G6 3.3 and later versions. it can be any value you want
         name: "text-shape",
-        // 设置 draggable 以允许响应鼠标的图拽事件
-        draggable: true
+        draggable: true,
       })
     }
 
     return keyShape
-  }
+  },
 })
 
 const data = {
   nodes: [
     {
       id: "node1",
-      size: [20, 20],
+      size: [round_w, round_h],
       label: "萝卜",
-      r: 3,
+      r: node_radius,
       type: "round",
-      color: "black"
+      color: "red",
     },
     {
       id: "node2",
@@ -242,7 +258,7 @@ const data = {
       state: "DOING",
       r: 20,
       type: "work-node",
-      color: "black"
+      color: "red",
     },
     {
       id: "node3",
@@ -250,69 +266,79 @@ const data = {
       state: "UNDO",
       r: 20,
       type: "work-node",
-      color: "black"
+      color: "red",
     },
     {
       id: "node4",
       label: "牛腩",
-      size: [20, 20],
-      r: 3,
+      size: [round_w, round_h],
+      r: node_radius,
       type: "round",
-      color: "black"
+      color: "red",
     },
     {
       id: "node5",
       label: "清洗",
       state: "DONE",
-      size: [20, 20],
       r: 20,
       type: "work-node",
-      color: "black"
+      color: "red",
     },
     {
       id: "node6",
       label: "潲水",
       state: "DONE",
-      size: [20, 20],
       r: 20,
       type: "work-node",
-      color: "black"
+      color: "red",
     },
     {
       id: "node7",
+      label: "大火慢炖",
+      state: "UNDO",
+      r: 20,
+      type: "work-node",
+      color: "red",
+    },
+    {
+      id: "node8",
       label: "萝卜牛腩",
-      size: [20, 20],
-      r: 3,
+      size: [round_w, round_h],
+      r: node_radius,
       type: "round",
-      color: "black"
-    }
+      color: "red",
+    },
   ],
   edges: [
     {
       source: "node1",
-      target: "node2"
+      target: "node2",
     },
     {
       source: "node2",
-      target: "node3"
+      target: "node3",
     },
     {
       source: "node3",
-      target: "node7"
+      target: "node7",
     },
     {
       source: "node4",
-      target: "node5"
+      target: "node5",
     },
     {
       source: "node5",
-      target: "node6"
+      target: "node6",
     },
     {
       source: "node6",
-      target: "node7"
-    }
-  ]
+      target: "node7",
+    },
+    {
+      source: "node7",
+      target: "node8",
+    },
+  ],
 }
 
 const graph = new G6.Graph({
@@ -320,25 +346,25 @@ const graph = new G6.Graph({
   width: 500,
   height: 500,
   modes: {
-    default: ["drag-canvas"]
+    default: ["drag-canvas", "zoom-canvas"],
   },
   defaultEdge: {
     type: "polyline",
     style: {
-      lineWidth: 3
-    }
+      lineWidth: 3,
+    },
   },
   defaultNode: {
     anchorPoints: [
       [0.5, 1],
-      [0.5, 0]
-    ]
+      [0.5, 0],
+    ],
   },
   layout: {
     type: "dagre",
     rankdir: "TB",
-    ranksep: 20
-  }
+    ranksep: 20,
+  },
 })
 graph.data(data)
 graph.render()
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,4 @@
+// test string
 const path_string =
   "M113,34.1V8c0-1.2-1.4-1.8-2.2-0.9L74.8,43c-0.5,0.5-0.5,1.4,0,1.9l36,36c0.9,0.9,2.2,0.2,2.2-0.9V55.1  c0-0.7,0.6-1.3,1.4-1.3c31.7,0.7,57.4,26.9,57.4,58.7c0,9.4-2.7,18.9-6.5,26.6c-0.2,0.5-0.2,1.1,0.2,1.5l12.8,12.8  c0.6,0.6,1.7,0.5,2.1-0.3c6.4-12.3,10.9-25.5,10.9-40.5C191.3,69.4,156.1,34.1,113,34.1L113,34.1z"
 // MoveTo: M, m
@@ -30,33 +31,23 @@ const key = {
   z: "z"
 }
 
-export function transferG6Path(path, ratio, ox = 0, oy = 0) {
+export function transferG6Path(path, ratio) {
   const pathArray = []
 
   let pathArrayIndex = -1
   let strNumber = ""
-  let _ox = 0
-  let _oy = 0
-
-  let cur = ''
-  let Mtime = 0 
 
   for (let i = 0; i < path.length; i++) {
     const str = path[i]
 
     const needNew = !!key[str]
     if (needNew) {
-      cur = needNew
-      if (strNumber) {
+      if (strNumber.trim() !== '') {
+        if(key[str] === 'M') {
+          console.log({strNumber})
+        }
         pathArray[pathArrayIndex].push(Number(strNumber) * ratio)
         strNumber = ""
-      }
-      if(needNew === 'M') {
-        _ox = ox,
-        _oy = oy
-      } else {
-        ox = 0,
-        oy = 0
       }
       pathArrayIndex++
       let arr = []
@@ -65,7 +56,7 @@ export function transferG6Path(path, ratio, ox = 0, oy = 0) {
     } else {
       if (str === "," || str === "-") {
         if (strNumber) {
-          pathArray[pathArrayIndex].push(Number(strNumber) * ratio - ox)
+          pathArray[pathArrayIndex].push(Number(strNumber) * ratio)
         }
         if (str === "-") {
           strNumber = "-"
